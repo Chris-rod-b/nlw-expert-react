1c780e35fd6363f5826b088db1117ad05c195645
Christian Rodrigues Batista <chris.rod_batista@outlook.com>
2024-02-07 10:21:43 -0300

    Init: Starting a new project with Vite

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import logo from './assets/logo-nlw-expert.svg';
+
+export function App() {
+  return (
+    <div>
+      <img src={logo}/>
+      <input type="text" placeholder='Busque em suas notas...'/>
+    </div>
+  )
+}
