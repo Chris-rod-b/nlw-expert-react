abe27b03111163b7d8e6334891a9bbc2ef86ffdf
Christian Rodrigues Batista <chris.rod_batista@outlook.com>
2024-02-07 18:39:31 -0300

    Feat: Building some compenents and stylesheet

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import logo from './assets/logo-nlw-expert.svg';
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
 
 export function App() {
   return (
@@ -17,52 +19,13 @@ export function App() {
       <div className="h-px bg-slate-700"/>
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3"> 
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-          </p>
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative"> 
-          <span className="text-sm font-medium text-slate-300">
-            Há 2 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae blanditiis, nihil asperiores tempore nostrum optio voluptatem deleniti saepe perferendis repellendus mollitia neque, quod fuga eum. Tenetur animi temporibus porro laboriosam.
-          </p>
-
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 
-            bg-gradient-to-t from-black/60 to-transparent pointer-events-none"/>
-        </div>
-        
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative"> 
-          <span className="text-sm font-medium text-slate-300">
-            Há 4 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam cum vitae, dicta velit omnis inventore? At, quaerat odit a libero laboriosam, saepe nisi fugiat placeat similique, accusantium molestiae obcaecati culpa?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam sed saepe rerum ipsa accusamus voluptatum facere quibusdam quod? Ex exercitationem corporis saepe nesciunt! Exercitationem quasi dicta alias laboriosam quia.
-          </p>
-
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 
-            bg-gradient-to-t from-black/60 to-transparent pointer-events-none"/>
-        </div>
-
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative"> 
-          <span className="text-sm font-medium text-slate-300">
-            Há 7 dias
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur a dolore perspiciatis consequatur quibusdam cumque ad, adipisci sint. Doloremque obcaecati enim dolorem, eaque numquam ullam impedit cum atque commodi animi.
-          </p>
-
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 
-            bg-gradient-to-t from-black/60 to-transparent pointer-events-none"/>
-        </div>
-
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />        
+        <NoteCard />
       </div>
     </div>
   )
---
+++ src/components/note-card.tsx
@@ -0,0 +1,20 @@
+export function NoteCard() {
+    return (
+        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 
+            overflow-hidden relative hover:ring-2 hover:ring-slate-600 
+            transition-shadow outline-none focus-visible:ring-2
+            focus-visible:ring-lime-400"> 
+                <span className="text-sm font-medium text-slate-300">
+                    Há 8 dias
+                </span>
+                <p className="text-sm leading-6 text-slate-400">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque minus 
+                    cum dolores repudiandae possimus nihil non fugit omnis in, optio aspernatur 
+                    nemo quibusdam mollitia totam eos aliquam rerum, minima deserunt.
+                </p>
+
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 
+                    bg-gradient-to-t from-black/60 to-transparent pointer-events-none"/>
+        </button>
+    )
+}
